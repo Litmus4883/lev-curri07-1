6219532d716f58a2b0cc13e55bd608a1042f0fc2
Litmus4883 <taigatomonaga@gmail.com>
2023-10-20 12:08:58 +0900

    lc09-2 リレーション

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,7 +14,7 @@
         <div class="content">
             <div class="content_post">
                 <h3>本文</h3>
-                <a href="">{{ $post->category->name }}</a>
+                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
                 <p>{{ $post->body }}</p>
             </div>
         </div>
